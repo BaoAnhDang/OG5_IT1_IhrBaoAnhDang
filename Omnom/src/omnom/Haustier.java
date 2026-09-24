8499511935e19cbe630721550f2f51f48bae26a1
CoiMinh <CoiMinh@DESKTOP-1O4IOSJ.fritz.box>
2026-09-24 15:14:29 +0200

    Zusatz: Omnom implementieren, Aufgabe 7 implementiert

--- Omnom/src/omnom/Haustier.java
+++ Omnom/src/omnom/Haustier.java
@@ -7,34 +7,42 @@ public class Haustier {
 	int zufrieden = 100;
 	int gesund = 100;
 	String name;
-	
+
 	public Haustier(String name) {
 		this.name = name;
 	}
-	
+
 	public int getHunger() {
 		return hunger;
 	}
 	public void setHunger(int hunger) {
-		this.hunger = hunger;
+		if (hunger >= 0 && hunger<= 100) {
+			this.hunger = hunger;
+		}
 	}
 	public int getMuede() {
 		return muede;
 	}
 	public void setMuede(int muede) {
-		this.muede = muede;
+		if (muede >= 0 && muede <= 100) {
+			this.muede = muede;
+		}
 	}
 	public int getZufrieden() {
 		return zufrieden;
 	}
 	public void setZufrieden(int zufrieden) {
-		this.zufrieden = zufrieden;
+		if ( zufrieden >= 0 && zufrieden <= 100) {
+			this.zufrieden = zufrieden;
+		}
 	}
 	public int getGesund() {
 		return gesund;
 	}
 	public void setGesund(int gesund) {
-		this.gesund = gesund;
+		if (gesund >= 0 && gesund <= 100 ) {
+			this.gesund = gesund;
+		}
 	}
 	public String getName() {
 		return name;
@@ -42,17 +50,17 @@ public class Haustier {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public void fuettern(int anzahl) {
-		
+		setHunger(hunger + 10);
 	}
 	public void schlafen(int dauer) {
-		
+		setMuede (muede + 10);
 	}
 	public void spielen(int dauer) {
-		
+		setZufrieden (zufrieden + 10);
 	}
 	public void heilen() {
-		
+		this.gesund = 100;
 	}
 }
